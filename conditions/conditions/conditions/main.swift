8b3a6ee1bc9b30f0dd12d078cfdf1568145dba33
larisol.corretora@gmail.com <larissamartinscorrea_noite@MacBook-Pro-de-LDS.local>
2024-06-19 19:31:39 -0300

    primeira questão do leetcode

---
+++ conditions/conditions/conditions/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  conditions
+//
+//  Created by Larissa Martins Correa on 19/06/24.
+//
+
+import Foundation
+
+class Solution {
+
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    for nums in nums
+    
+    }
+}
+
+struct Result {
+    let nums
+}
+
+// Não pode usar o mesmo elemento duas vezes.
+// Os números somados é a saída.
+
+
