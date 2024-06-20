b076c24f78a024f6e240b0c6df2e6e41589cca3f
larisol.corretora@gmail.com <larissamartinscorrea_noite@MacBook-Pro-de-LDS.local>
2024-06-19 21:08:16 -0300

    mais uma tentativa

--- conditions/conditions/conditions/main.swift
+++ conditions/conditions/conditions/main.swift
@@ -5,22 +5,35 @@
 //  Created by Larissa Martins Correa on 19/06/24.
 //
 
-import Foundation
-
 class Solution {
 
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     
-        for (num in nums, num <= nums, where num != nums) {
-            print(nums)
-        }
+        var num: [Int] = [Int]()
+        var a = 0
+        var b = 0
+
+        for (a in num && a < nums.count - 1) {
+            b = a + 1
+            while b <= nums.count - 1{
+                if (nums[a] + nums[b] == target){
+                num.append(a)
+                num.append(b)
+                    break
+            }
+            a += 1
+            }
+        a = a + 1
+    }
+    return num
     
     }
 }
 
 
+
+
 // Não pode usar o mesmo elemento duas vezes.
 // Os números somados é a saída.
 // Se i for igual a 0 e o comprimento de i for menor
-
 
