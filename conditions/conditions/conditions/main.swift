23aa2ee31b26f49ce7a19bdaa7e2ea7c0d8548a8
Larissa Corrêa <101229830+larimscorrea@users.noreply.github.com>
2024-06-21 18:11:13 -0300

    Update main.swift

--- conditions/conditions/conditions/main.swift
+++ conditions/conditions/conditions/main.swift
@@ -38,6 +38,40 @@
 //    }
 //}
 
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    // Loop passa cada elemento no array usando o loop for.
+    for i in 0..<nums.count {
+        // Pega o elemento complementar do elemento atual.
+        let complement = target - nums[i]
+        
+        // Usa o loop while para pesquisar pelo complemento no elemento restante.
+        var j = i + 1
+        while j < nums.count {
+            if nums[j] == complement {
+                return [i, j]
+            }
+            j += 1
+        }
+    }
+    return []
+}
+
+// Exemplo 1
+let nums1 = [2, 7, 11, 15]
+let target1 = 9
+print(twoSum(nums1, target1))  // Saída: [0, 1]
+
+// Exemplo 2
+let nums2 = [3, 2, 4]
+let target2 = 6
+print(twoSum(nums2, target2))  // Saída: [1, 2]
+
+// Exemplo 3
+let nums3 = [3, 3]
+let target3 = 6
+print(twoSum(nums3, target3))  // Saída: [0, 1]
+
+
 class Operacoes {
     func soma(num1:Int,num2:Int) -> Int {
         var result: Int
